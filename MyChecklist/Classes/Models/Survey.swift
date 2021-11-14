50bd14b6388bcac41d988ed02136cb8e1f8800ea
BashArslanaliev <bashir.arslanaliev@workspad.com>
2021-11-14 19:50:56 +0300

    modified: implemented Codable protocol for Survey model

--- MyChecklist/Classes/Models/Survey.swift
+++ MyChecklist/Classes/Models/Survey.swift
@@ -14,8 +14,8 @@ struct Survey: Codable {
     var weight: Int = 70
     var smoke: Smoke?
     var alcohol: Alcohol?
-    var bloodPressureTop: Int?
-    var bloodPressureLow: Int?
+    var bloodPressureTop: Int = 140
+    var bloodPressureLow: Int = 90
     var bpUndefined = false
     var hasHighCholesterol = false // high cholesterol
     var hasDiabetes  = false // diabetes
@@ -32,22 +32,109 @@ struct Survey: Codable {
     var hasHiv = false // Hiv
     var hasProstateCancer = false // prostateCancer
     var hasCervicalCancerWoman = false // cervicalCancerWoman
-    var hasColonCancer = false // colonCancer + gender
-    var hasStomachCancer = false // stomachCancer + gender
-    var hasLungsCancer = false // lungsCancer + gender
-    var hasMelanoma = false // melanoma + gender
+    var hasColonCancerMan = false // colonCancerMan
+    var hasColonCancerWoman = false // colonCancerWoman
+    var hasStomachCancerMan = false // stomachCancerMan
+    var hasStomachCancerWoman = false // stomachCancerWoman
+    var hasLungsCancerMan = false // lungsCancerMan
+    var hasLungsCancerWoman = false // lungsCancerWoman
+    var hasMelanomaMan = false // melanomaMan
+    var hasMelanomaWoman = false // melanomaWoman
     
-    var prostateDetails: ProstateCancerDetails? // prostateCancerDetails
-    var stomachDetails: StomachCancerDetails? // stomachCancerDetails
-    var colonDetails: ColonCancerDetails? // colonCancerDetails
+    var prostateCancerDetails: ProstateCancerDetails? // prostateCancerDetails
+    var stomachCancerDetails: StomachCancerDetails? // stomachCancerDetails
+    var colonCancerDetails: ColonCancerDetails? // colonCancerDetails
     
-//    enum CodingKeys: String, CodingKey {
-//        case gender, age, height
-//        case weight, smoke, alcohol
-//        case bloodPressure
-//        case bpUndefined = "bp-undefined"
-//        case highCholesterol = "high cholesterol"
-//    }
+    enum CodingKeys: String, CodingKey {
+        case gender, age, height, weight
+        case smoke, alcohol
+        case bloodPressureTop, bloodPressureLow
+        case prostateCancerDetails, stomachCancerDetails, colonCancerDetails
+        case bpUndefined = "bp-undefined"
+        case hasHighCholesterol = "high cholesterol"
+        case hasDiabetes = "diabetes"
+        case hasBrokenBone = "brokeBone"
+        case hasFamilyInfarct = "Stroke"
+        case hasFamilyInsult = "insult"
+        case hasFamilyRelativeHipBroke = "relativeHipBroke"
+        case hasFamilyDiabetis = "familyDiabetis"
+        case hasChronicLungs = "chronicLungs"
+        case hasChronicHeart = "chronicHeart"
+        case hasChronicLiver = "chronicLiver"
+        case hasChronicStomach = "chronicStomach"
+        case hasChronicKidneys = "chronicKidneys"
+        case hasHiv = "Hiv"
+        case hasProstateCancer = "prostateCancer"
+        case hasCervicalCancerWoman = "cervicalCancerWoman"
+        case hasColonCancerMan = "colonCancerMan"
+        case hasColonCancerWoman = "colonCancerWoman"
+        case hasStomachCancerMan = "stomachCancerMan"
+        case hasStomachCancerWoman = "stomachCancerWoman"
+        case hasLungsCancerMan = "lungsCancerMan"
+        case hasLungsCancerWoman = "lungsCancerWoman"
+        case hasMelanomaMan = "melanomaMan"
+        case hasMelanomaWoman = "melanomaWoman"
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: Self.CodingKeys)
+        
+        try container.encode(gender, forKey: .gender)
+        try container.encode(age, forKey: .age)
+        try container.encode(weight, forKey: .weight)
+        try container.encode(height, forKey: .height)
+        
+        if let smoke = smoke {
+            try container.encode(smoke, forKey: .smoke)
+        }
+        
+        if let alcohol = alcohol {
+            try container.encode(alcohol, forKey: .alcohol)
+        }
+        
+        if bpUndefined {
+            try container.encode("true", forKey: .bpUndefined)
+        } else {
+            try container.encode(bloodPressureTop, forKey: .bloodPressureTop)
+            try container.encode(bloodPressureLow, forKey: .bloodPressureLow)
+        }
+        
+        try container.encode(["\(hasHighCholesterol)"], forKey: .hasHighCholesterol)
+        try container.encode(["\(hasDiabetes)"], forKey: .hasDiabetes)
+        try container.encode(["\(hasBrokenBone)"], forKey: .hasBrokenBone)
+        try container.encode(["\(hasFamilyInsult)"], forKey: .hasFamilyInsult)
+        try container.encode(["\(hasFamilyInfarct)"], forKey: .hasFamilyInfarct)
+        try container.encode(["\(hasFamilyRelativeHipBroke)"], forKey: .hasFamilyRelativeHipBroke)
+        try container.encode(["\(hasFamilyDiabetis)"], forKey: .hasFamilyDiabetis)
+        try container.encode(["\(hasChronicLungs)"], forKey: .hasChronicLungs)
+        try container.encode(["\(hasChronicHeart)"], forKey: .hasChronicHeart)
+        try container.encode(["\(hasChronicLiver)"], forKey: .hasChronicLiver)
+        try container.encode(["\(hasChronicStomach)"], forKey: .hasChronicStomach)
+        try container.encode(["\(hasChronicKidneys)"], forKey: .hasChronicKidneys)
+        try container.encode(["\(hasHiv)"], forKey: .hasHiv)
+        try container.encode(["\(hasProstateCancer)"], forKey: .hasProstateCancer)
+        try container.encode(["\(hasCervicalCancerWoman)"], forKey: .hasCervicalCancerWoman)
+        try container.encode(["\(hasColonCancerMan)"], forKey: .hasColonCancerMan)
+        try container.encode(["\(hasColonCancerWoman)"], forKey: .hasColonCancerWoman)
+        try container.encode(["\(hasStomachCancerMan)"], forKey: .hasStomachCancerMan)
+        try container.encode(["\(hasStomachCancerWoman)"], forKey: .hasStomachCancerWoman)
+        try container.encode(["\(hasLungsCancerMan)"], forKey: .hasLungsCancerMan)
+        try container.encode(["\(hasLungsCancerWoman)"], forKey: .hasLungsCancerWoman)
+        try container.encode(["\(hasMelanomaMan)"], forKey: .hasMelanomaMan)
+        try container.encode(["\(hasMelanomaWoman)"], forKey: .hasMelanomaWoman)
+        
+        if let prostateCancerDetails = prostateCancerDetails {
+            try container.encode(prostateCancerDetails.rawValue, forKey: .prostateCancerDetails)
+        }
+        
+        if let stomachCancerDetails = stomachCancerDetails {
+            try container.encode(stomachCancerDetails.rawValue, forKey: .stomachCancerDetails)
+        }
+        
+        if let colonCancerDetails = colonCancerDetails {
+            try container.encode(colonCancerDetails.rawValue, forKey: .colonCancerDetails)
+        }
+    }
 }
 
 enum Gender: String, Codable {
@@ -72,13 +159,13 @@ struct BloodPressure: Codable {
     var bloodPressureLow: Int = 80
 }
 
-enum ProstateCancerDetails: Codable {
+enum ProstateCancerDetails: String, Codable {
     case morethan1
     case under65
     case over65
 }
 
-enum StomachCancerDetails: Codable {
+enum StomachCancerDetails: String, Codable {
     case morethan3
     case under40
     case under50
@@ -86,7 +173,7 @@ enum StomachCancerDetails: Codable {
     case none
 }
 
-enum ColonCancerDetails: Codable {
+enum ColonCancerDetails: String, Codable {
     case lessthan60
     case manyRelatives
     case over60
