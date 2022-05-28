965d241a7defef76a62184e037ffd0e783954a4e
BashArslanaliev <bashir.arslanaliev@workspad.com>
2022-05-28 22:53:41 +0300

    Added constants

---
+++ MyChecklist/Classes/BusinessLogic/Network/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MyChecklist
+//
+//  Created by Башир Арсланалиев on 07.10.2021.
+//
+
+import Foundation
+
+let host = "infinite-brook-17660.herokuapp.com"
+
+let appBaseUrl = "https://\(host)/"
